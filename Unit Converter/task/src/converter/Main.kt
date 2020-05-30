044d888f9f3720bdfa9f6e3e601f47c089732a59
alicenychto <irol2273@icloud.com>
2020-05-30 16:58:56 +0200

    stage 3

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,28 +1,44 @@
 package converter
 
-val conversion: List<Double> = listOf<Double>(1.0, 1000.0, 0.01, 0.001, 1609.35, 0.9144, 0.3048, 0.0254)
-val units = listOf(
-        listOf("meter", "meters"), listOf("kilometer", "kilometers"), listOf("centimeter", "centimeters"),
-        listOf("millimeter", "millimeters"), listOf("mile", "miles"), listOf("yard", "yards"),
-        listOf("foot", "feet"), listOf("inch", "inches")
-)
+enum class Measures(val factor: Double, val names: List<String>) {
+    GRAM(1.0, listOf("g", "gram", "grams")),
+    KILOGRAM(1000.0, listOf("kg", "kilogram", "kilograms")),
+    MILLIGRAM(0.001, listOf("mg", "milligram", "milligrams")),
+    POUNDS(453.592, listOf("lb", "pound", "pounds")),
+    OUNCE(28.3495, listOf("oz", "ounce", "ounces")),
+
+    METER(1.0, listOf("m", "meter", "meters")),
+    KILOMETER(1000.0, listOf("km", "kilometer", "kilometers")),
+    CENTIMETER(0.01, listOf("cm", "centimeter", "centimeters")),
+    MILLIMETER(0.001, listOf("mm", "millimeter", "millimeters")),
+    MILE(1609.35, listOf("mi", "mile", "miles")),
+    YARD(0.9144, listOf("yd", "yard", "yards")),
+    FOOT(0.3048, listOf("ft", "foot", "feet")),
+    INCH(0.0254, listOf("in", "inch", "inches"));
+
+    companion object {
+        fun getMeasures(measure: String): Measures {
+            for (x in values())
+                if (x.names.contains(measure))
+                    return x
+            throw Exception("Unknown conversion unit.")
+        }
+    }
+}
 
 fun main() {
-    println("Enter a number and a measure of length:")
-    val line = readLine()
-    val s = line!!.split(" ")
-    val conv = when (s[1].toLowerCase()) {
-        "m", "meter", "meters" -> 0
-        "km", "kilometer", "kilometers" -> 1
-        "cm", "centimeter", "centimeters" -> 2
-        "mm", "millimeter", "millimeters" -> 3
-        "mi", "mile", "miles" -> 4
-        "yd", "yard", "yards" -> 5
-        "ft", "foot", "feet" -> 6
-        "in", "inch", "inches" -> 7
-        else -> throw Exception("wrong input!")
+    println("Enter what you want to convert (or exit):")
+    var input = readLine()
+    while (input != "exit") {
+        val lines = input!!.split(" ")
+        val start = Measures.getMeasures(lines[1].toLowerCase())
+        val end = Measures.getMeasures(lines[3].toLowerCase())
+        val converted = lines[0].toDouble() * start.factor / end.factor
+        val startWord = if (lines[0].toDouble() == 1.0) start.names[1] else start.names[2]
+        val endWord = if (converted == 1.0) end.names[1] else end.names[2]
+
+        println("${lines[0].toDouble()} $startWord is $converted $endWord")
+        println("Enter what you want to convert (or exit):")
+        input = readLine()
     }
-    val y = s[0].toDouble()
-    val x = y * conversion[conv]
-    println("$y " + (if (y == 1.0) units[conv][0] else units[conv][1]) + " is $x " + if (x == 1.0) "meter" else "meters")
 }
